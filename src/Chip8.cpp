3938b2154f6d978d801c9f7e66ddedcec86e932d
Juan Estrada <juanzatch17@hotmail.com>
2025-04-28 17:54:16 -0400

    Added load rom function

--- src/Chip8.cpp
+++ src/Chip8.cpp
@@ -1,5 +1,6 @@
 #include "Chip8.h"
-
+#include <iostream>
+#include <fstream> 
 Chip8::Chip8() {
     initialize();
 };
@@ -18,11 +19,52 @@ void Chip8::initialize(){
     sp = 0x00;
     opcode = 0x00; 
     // TODO => Font 
+    // TODO => Fontset initialized at memory location 0x50 == 80 and onwards .
     // TODO => sound/timers 
 }
 
-void Chip8::loadRom(){
+// filename is pointer to a read-only / const char 
+bool Chip8::loadRom(const char * filename){
+    // Initialize 
+    initialize();
 
+    //open in binary mode, ate sets the stream's position to the end on opening. 
+    
+    std::ifstream rom_file(filename, std::ios::binary | std::ios::ate);
+
+    if(!rom_file){
+        std::cerr << "Failed to open the file."; 
+        return false; 
+    }
+    //filesize 
+    std::streamsize rom_size = file.tellg(); 
+    std::cout << "Rom Size is: " << rom_size << std::endl; 
+    // seekg + beg, move to x bytes from beginning 
+    rom_file.seekg(0, std::ios::beg); // rewind 
+
+    // buffer / mem allocation 
+    std::vector<char> buffer(file_size); 
+
+    // reads from file and adds to buffer, data gives access to the internal vector array. We get a pointer to the first element in the vector.
+    if(!rom_file.read(buffer.data(), file_size)){
+        std::cerr << "Failed to read the file.\n";
+        return false; 
+    } else{
+        std::cout << "File read and added to buffer successfully." << std::endl;
+    };
+
+    // Store elements from buffer into chip 8 memory 
+    // we loop through the memory array, starting at 512 in memory and while i is less than the size of the buffer
+    // check if the file size is not bigger than the available memory size. 
+    if(file_size < (4096 - 512)){
+        for(int i = 0; i < buffer.size(); ++i){
+            memory[512 + i] = buffer[i];
+        }
+    }
+
+    rom_file.close();
+    return true;
+    // free(buffer) not needed vector handles this. 
 }
 
 
